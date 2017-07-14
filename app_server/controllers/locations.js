749e1a1c73cfc7a04315414b8a83e686e3b7f3a2
Pablo <prvillagra@gmail.com>
2017-07-13 21:33:30 -0300

    Repeaters

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,30 @@
 module.exports.homelist = function (req, res) {
-    res.render('location/location-list', { title: 'Home' });
+    res.render('location/location-list', {
+        title: 'Home',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [{                                             
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        },{
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        },{
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium wifi'],
+            distance: '250m'
+        }]
+    });
 };
 
 module.exports.locationInfo = function (req, res) {
